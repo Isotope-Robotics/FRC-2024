7773e85c96c40a89fef06793c655c95c5d812a2c
Ethen <ebot14rocks@gmail.com>
2024-01-21 17:51:52 -0500

    removed selectable chooser

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,8 +24,7 @@ import frc.robot.Subsystems.Swerve;
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
 
   //Swerve Varibles
   public static final CTREConfigs ctreConfigs = new CTREConfigs();
@@ -43,9 +42,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+   
+
 
     //Zero Gyro Heading for Swerve
     swerve.zeroHeading();
@@ -77,23 +75,13 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+  
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+   
   }
 
   /** This function is called once when teleop is enabled. */
--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -19,28 +19,30 @@ public class Intake {
     public static DigitalInput noteIntaked;
     public static DigitalInput wristLimit;
 
-    public static final PIDController wristPID = new PIDController(Constants.Intake.kP, Constants.Intake.kI, Constants.Intake.kD);
+    public static final PIDController wristPID = new PIDController(Constants.Intake.kP, Constants.Intake.kI,
+            Constants.Intake.kD);
 
     public Intake(int wristMotorCANID, int intakeMotorCANID) {
-//Motor Declarations
+
+        // Motor Declarations
         wristMotor = new CANSparkMax(wristMotorCANID, MotorType.kBrushless);
         intakeMotor = new CANSparkMax(intakeMotorCANID, MotorType.kBrushless);
-        
-        //Idle Mode Declarations
+
+        // Idle Mode Declarations
         wristMotor.setIdleMode(Constants.Intake.Brake);
         intakeMotor.setIdleMode(Constants.Intake.Coast);
 
-        //Limit Switch (Photo Eye) Declarations
+        // Limit Switch (Photo Eye) Declarations
         noteIntaked = new DigitalInput(8);
         wristLimit = new DigitalInput(0);
-    
-        //Encoders Declarations
-        wristEncoder = wristMotor.getEncoder(SparkRelativeEncoder.Type.kHallSensor, Constants.Encoders.NEO_ENCODER_COUNTS);
 
-       
+        // Encoders Declarations
+        wristEncoder = wristMotor.getEncoder(SparkRelativeEncoder.Type.kHallSensor,
+                Constants.Encoders.NEO_ENCODER_COUNTS);
+
     }
 
-    //Photoelectric Sensor for sensing a note in the intake
+    // Photoelectric Sensor for sensing a note in the intake
     public boolean getNoteIntaked() {
         if (noteIntaked.get()) {
             return true;
@@ -49,7 +51,7 @@ public class Intake {
         }
     }
 
-//Returns Limit Switch for Wrist Limit
+    // Returns Limit Switch for Wrist Limit
     public boolean getWristLimit() {
         if (wristLimit.get()) {
             return true;
@@ -58,38 +60,37 @@ public class Intake {
         }
     }
 
-//Stops intake motors
+    // Stops intake motors
     public void intakeStop() {
         intakeMotor.set(0);
     }
 
-//Stops wrist motors
+    // Stops wrist motors
     public void wristStop() {
         wristMotor.set(0);
     }
 
-    public void zeroEncoders(){
-         //Zero Out Encoder Positions
+    public void zeroEncoders() {
+        // Zero Out Encoder Positions
         wristEncoder.setPosition(0.0);
         System.err.println("Zeroed Intake Encoders");
     }
 
-//Wrist up movement control
+    // Wrist up movement control
     public void wristUp() {
         if (getWristLimit()) {
             wristStop();
-        }
-        else {
+        } else {
             intakeMotor.set(wristPID.calculate(wristEncoder.getPosition(), 0));
         }
     }
 
-//Wrist down movement control
+    // Wrist down movement control
     public void wristDown() {
         intakeMotor.set(wristPID.calculate(wristEncoder.getPosition(), 60));
     }
 
-//Intake speed set
+    // Intake speed set
     public void intakeStart(double speed) {
         intakeMotor.set(speed);
     }
