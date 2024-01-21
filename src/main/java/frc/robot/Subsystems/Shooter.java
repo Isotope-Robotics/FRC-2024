de37cae1d1de7cefa9102e9bdf72a713fdc1623f
Ethen <ebot14rocks@gmail.com>
2024-01-21 16:45:33 -0500

    removed useless varible shooter function as it serves no purpose rn

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -10,7 +10,7 @@ import com.revrobotics.SparkRelativeEncoder;
 
 import frc.robot.Constants;
 
-public class Shooter  {
+public class Shooter {
 
     private CANSparkMax shooterMotor1;
     private CANSparkMax shooterMotor2;
@@ -18,24 +18,24 @@ public class Shooter  {
     public static RelativeEncoder shooter2Encoder;
 
     public Shooter() {
-        //Motor Declarations
+        // Motor Declarations
         shooterMotor1 = new CANSparkMax(Constants.Shooter.shooterMotor1ID, MotorType.kBrushless);
         shooterMotor2 = new CANSparkMax(Constants.Shooter.shooterMotor2ID, MotorType.kBrushless);
 
-        //Idle Mode Declarations
+        // Idle Mode Declarations
         shooterMotor1.setIdleMode(Constants.Shooter.Brake);
         shooterMotor2.setIdleMode(Constants.Shooter.Brake);
 
-        //Encoders Declarations
+        // Encoders Declarations
         shooter1Encoder = shooterMotor1.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 4096);
         shooter2Encoder = shooterMotor2.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 4096);
 
-        //Zero Out Encoder Positions
+        // Zero Out Encoder Positions
         shooter1Encoder.setPosition(0);
         shooter2Encoder.setPosition(0);
     }
 
-    //Set both shooter motors to shoot (adjust the speed as needed)
+    // Set both shooter motors to shoot (adjust the speed as needed)
     public void shoot(double leftSpeed, double rightSpeed) {
         shooterMotor1.set(leftSpeed);
         shooterMotor2.set(rightSpeed);
@@ -43,24 +43,16 @@ public class Shooter  {
         SmartDashboard.putNumber("Shooter 2 Speed", rightSpeed);
     }
 
-    //Varible shooter control loop
-    public void variableShooter(double motor1Speed, double motor2Speed){
-        shooterMotor1.set(motor1Speed);
-        shooterMotor2.set(motor2Speed);
-
-        SmartDashboard.putNumber("Shooter 1 Speed", motor1Speed);
-        SmartDashboard.putNumber("Shooter 2 Speed", motor2Speed);
-    }
-
-    //Stop both shooter motors
+    // Stop both shooter motors
     public void stop() {
         shooterMotor1.set(0);
         shooterMotor2.set(0);
         SmartDashboard.putNumber("Shooter is Stopped", 0);
     }
 
+    // Set both shooter motors to intake through the source (adjust the speed as
+    // needed)
     public void sourceIntake(double speed) {
-        // Set both shooter motors to intake through the source (adjust the speed as needed)
         shooterMotor1.set(-speed);
         shooterMotor2.set(-speed);
         SmartDashboard.putNumber("Shooter Speed", speed);
