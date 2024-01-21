0c87530b3e8a92ab1476fa8693d0578ce63facfe
Ethen <ebot14rocks@gmail.com>
2024-01-21 17:16:24 -0500

    added pathplanner basic config to the project

---
+++ src/main/java/frc/robot/Autos/test_auto.java
@@ -0,0 +1,18 @@
+package frc.robot.Autos;
+
+public class test_auto {
+    public void runToRight(){
+        try {
+
+            wait(10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    public void followPathToCenterLine(){
+        
+    }
+}
