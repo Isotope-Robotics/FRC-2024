03b5a8b00dce3c6bb6f16786d11cd963ab5c6dee
kyotothenerd <94712321+Devin-M5706@users.noreply.github.com>
2024-02-03 15:53:07 -0500

    added blinkin lights to stopping wrist motor

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,16 +1,13 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkRelativeEncoder;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import frc.robot.Constants;
 
 // import frc.robot.Subsystems.Blinkin;
@@ -81,6 +78,7 @@ public class Intake extends SubsystemBase {
     // Stops wrist motors
     public void wristStop() {
         wristMotor1.set(0);
+        blinkin.chaseRed();
         // wristMotor2.set(0);
     }
 
