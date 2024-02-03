321c0b45e2f0a7992a31e187d9a441d24f38624f
55133 <wypflannery@gmail.com>
2024-02-02 19:28:47 -0500

    Shoot Command

--- src/main/java/frc/robot/AutoCommands/ShooterCommands.java
+++ src/main/java/frc/robot/AutoCommands/ShooterCommands.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Subsystems.Shooter;
 
-public final class ShooterCommands {
+public class ShooterCommands {
     public static Shooter m_Shooter = Shooter.getInstance();
 
     private ShooterCommands(){
@@ -14,11 +14,18 @@ public final class ShooterCommands {
     }
 
     // why does this not work :(
-    // public static Command Shoot() {
-    //         return Commands.runOnce(() -> {
-    //             m_Shooter.shoot(0.0, 0.0);
-    //         }, m_Shooter);
-    //     }
+    public static Command Shoot() {
+            return Commands.run(() -> {
+                m_Shooter.shoot(0.0, 0.0);
+               // waitCommand(0.5);
+            }, m_Shooter);
+        }
+
+        public static Command DropToPickUp() {
+            return Commands.run(() -> {
+                m_Shooter.wristDown();
+            }, m_Shooter);
+        }
 
    
 
