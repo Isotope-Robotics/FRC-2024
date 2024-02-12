e9e978a10c1ab642f8e5cc6e8442873b03480edc
Ethen <ebot14rocks@gmail.com>
2024-02-11 22:01:21 -0500

    removed NavX lib

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,11 +18,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-<<<<<<< Updated upstream
 import frc.robot.AutoCommands.IntakeCommands;
 import frc.robot.Subsystems.Climber;
-=======
->>>>>>> Stashed changes
 import frc.robot.Subsystems.Intake;
 import frc.robot.Subsystems.Shooter;
 import frc.robot.Subsystems.Swerve;
